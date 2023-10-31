7ada4881d1ecc76a92f4242a3cb6c4af99b935fe
Ndollem <ndollem@gmail.com>
2023-10-31 10:42:13 +0700

    update get ip

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const hbs = require('hbs');
-const ip = require('ip');
+const requestIp = require('request-ip');
 
 app.set('view engine', 'hbs');
 
@@ -16,7 +16,7 @@ app.get('/demo-page1.html', (req, res) => {
     const data = {
         title: "Program hilirisasi pemerintah",
         content: "Hilirisasi yang ingin kita lakukan adalah hilirisasi yang melakukan transfer teknologi yang memanfaatkan sumber energi baru dan terbarukan, serta meminimalisir dampak lingkungan",
-        ip: ip.address,
+        ip: req.header('x-forwarded-for'),
         url: 'demo-page1'
     };
 
@@ -26,7 +26,7 @@ app.get('/demo-page2.html', (req, res) => {
     const data = {
         title: "Bumbu sate ala restoran",
         content: "Bumbu sate menjadi salah satu bahan masakan yang banyak dicari. Sate sendiri adalah sajian khas nusantara yang telah dikenal hingga ke mancanegara. Sate merupakan potongan daging ayam hingga kambing yang ditata di atas tusuk lidi ditambah dengan serangkaian bumbu.",
-        ip: ip.address,
+        ip: req.header('x-forwarded-for'),
         url: 'demo-page2'
     };
 
@@ -36,7 +36,7 @@ app.get('/demo-page3.html', (req, res) => {
     const data = {
         title: "Sejarah Jembatan Siti Nurbaya",
         content: "Jembatan Siti Nurbaya merupakan jembatan cantik yang membentang sepanjang 156 meter di atas sunggai Batang Arau, Kecamatan Padang Selatan, Kota Padang, Sumatera Barat. Jembatan yang didominasi warna merah kuning itu juga menjadi ikonnya Kota Padang.",
-        ip: ip.address,
+        ip: requestIp.getClientIp(req),
         url: 'demo-page3'
     };
 
